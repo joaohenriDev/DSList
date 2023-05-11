9c37419f8aca583aaf2b1146c682bbf1eeef726b
João Henrique De Oliveira <joaohenri293@gmail.com>
2023-05-11 15:57:46 -0300

    GET lists

---
+++ src/main/java/com/joaoportfolio/dslist/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.joaoportfolio.dslist.services;
+
+import com.joaoportfolio.dslist.dto.GameListDTO;
+import com.joaoportfolio.dslist.entities.GameList;
+import com.joaoportfolio.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
